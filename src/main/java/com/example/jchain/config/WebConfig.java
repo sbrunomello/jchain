d678fbacfe7ac233e62e7762b203c07077846583
sbrunomello <sbrunomello@gmail.com>
2024-06-16 22:54:12 -0300

    CORS

---
+++ src/main/java/com/example/jchain/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.example.jchain.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://192.168.1.11:8100")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
